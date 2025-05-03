83082224dfe2bb0b7f64422745a72da9d7615e95
EzeErlicher <103656263+EzeErlicher@users.noreply.github.com>
2025-05-03 10:37:21 -0300

    Update create_image.sh

--- 1-bootable_image/create_image.sh
+++ 1-bootable_image/create_image.sh
@@ -1,8 +1,8 @@
-# Crear un archivo llamado main.img y escribir una secuencia específica de bytes
+# Crea una imagen llamada main.img
 printf '\364%509s\125\252' > main.img
 
-# Crear un archivo llamado a.S y escribir la instrucción "hlt" en él
+# Crea un archivo llamado a.S y escribe la instrucción "hlt" en él
 echo hlt > a.S
 
-# Ensamblar el archivo a.S y generar un archivo objeto llamado a.o
+# Crea el archivo assembly a.S y generar un archivo objeto llamado a.o
 as -o a.o a.S
